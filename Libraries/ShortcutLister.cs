2d66b5b9f31f93adb05405afc15244a63e850316
ykgoh <ykgoh@email.com>
2018-05-17 16:19:07 +0800

    ported from local repo.

---
+++ Libraries/ShortcutLister.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Libraries
+{
+    /// <summary>
+    /// This class contains functions which can be used in the application
+    /// </summary>
+    public class ShortcutLister
+    {
+        Control ctlControlToGenerate = null;
+
+        /// <summary>
+        /// Get shortcuts with option to select to save it to somewhere
+        /// </summary>
+        /// <param name="ctl"></param>
+        /// <param name="sb"></param>
+        /// <param name="ctlEventControl"></param>
+        public void GetShortcuts(Control ctl, Control ctlEventControl)
+        {
+            ctlControlToGenerate = ctl;
+            ctlEventControl.Click += new EventHandler(_GetShortcuts_Click);
+        }
+
+        /// <summary>
+        /// Get shortcuts with option to select to save it to somewhere
+        /// </summary>
+        /// <param name="ctl"></param>
+        /// <param name="ctlEventControl"></param>
+        public void GetShortcuts(Control ctl, ToolStripItem ctlEventControl)
+        {
+            ctlControlToGenerate = ctl;
+            ctlEventControl.Click += new EventHandler(_GetShortcuts_Click);
+        }
+
+        /// <summary>
+        /// Event for _GetShortcut_Click
+        /// Used by 
+        ///     GetShortcuts(Control ctl, Control ctlEventControl)
+        ///     GetShortcuts(Control ctl, ToolStripItem ctlEventControl)
+        ///     
+        /// These methods will add a click event to the control passed in as 
+        /// such when the button is clicked, it will get all the shortcuts
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void _GetShortcuts_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text File (*.txt)|*.txt";
+            sfd.Title = "Save shortcuts to";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                StringBuilder sb = new StringBuilder();
+                GetShortcuts(ctlControlToGenerate, ref sb);
+
+                using (System.IO.StreamWriter sw = new System.IO.StreamWriter(sfd.FileName))
+                {
+                    sw.WriteLine(sb.ToString());
+                    sw.Flush();
+                    sw.Close();
+                    sw.Dispose();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets all shortcuts in the control and write it to the stringbuilder which is passed in byref
+        /// </summary>
+        /// <param name="ctl"></param>
+        /// <param name="sb"></param>
+        public void GetShortcuts(Control ctl, ref StringBuilder sb)
+        {
+            //Checks for ContextMenu
+            if (ctl.ContextMenuStrip != null)
+            {
+                StringBuilder sbTmp = new StringBuilder();
+                GetShortcuts((ToolStrip)ctl.ContextMenuStrip, ref sbTmp);
+
+                if (!string.IsNullOrEmpty(sbTmp.ToString()))
+                {
+                    sb
+                       .AppendLine()
+                       .AppendLine(
+                            string.Format(
+                                "\nContext Menu [{0}]", 
+                                (string.IsNullOrEmpty(ctl.Text) ? ctl.Name : ctl.Text) + 
+                                "." + 
+                                ctl.ContextMenuStrip.Name)
+                            )
+                       .AppendLine(sbTmp.ToString());
+                }
+            }
+
+            if (ctl is ToolStrip)
+            {
+                //ToolStrip
+                StringBuilder sbTmp = new StringBuilder();
+                GetShortcuts((ToolStrip)ctl, ref sbTmp);
+
+                if (!string.IsNullOrEmpty(sbTmp.ToString()))
+                {
+                    sb
+                       .AppendLine()
+                       .AppendLine(string.Format("\n Menu [{0}]", ctl.Name))
+                       .AppendLine(sbTmp.ToString());
+                }
+            }
+
+            foreach (Control c in ctl.Controls)
+            {
+                if (c.Controls != null)
+                {
+                    GetShortcuts(c, ref sb);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets all shortcuts in the ToolStrip and write it to the stringbuilder which is passed in byref
+        /// to be used by the parent method
+        /// - GetShortcuts(Control ctl, ref StringBuilder sb)
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="sb"></param>
+        public void GetShortcuts(ToolStrip t, ref StringBuilder sb)
+        {
+            if (t.Items != null && t.Items.Count > 0)
+            {
+                foreach (ToolStripItem i in t.Items)
+                {
+                    if (i is ToolStripDropDownItem)
+                    {
+                        GetShortcuts((ToolStripDropDownItem)i, ref sb);
+                    }
+                    else if (i is ToolStripMenuItem)
+                    {
+                        GetShortcuts((ToolStripMenuItem)i, ref sb);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets all shortcuts in the ToolStripMenuItem and write it to the stringbuilder which is passed in byref
+        /// to be used by the parent method
+        /// - GetShortcuts(Control ctl, ref StringBuilder sb)
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="sb"></param>
+        public void GetShortcuts(ToolStripMenuItem t, ref StringBuilder sb)
+        {
+            if (t.DropDownItems != null && t.DropDownItems.Count > 0)
+            {
+                foreach (ToolStripItem i in t.DropDownItems)
+                {
+                    if (i is ToolStripMenuItem)
+                    {
+                        GetShortcuts((ToolStripMenuItem)i, ref sb);
+                    }
+                    else
+                    {
+                        AppendShortcut(t.ShortcutKeys.ToString(), t.Text, ref sb);
+                    }
+                }
+            }
+            else
+            {
+                AppendShortcut(t.ShortcutKeys.ToString(), t.Text, ref sb);
+            }
+        }
+
+        /// <summary>
+        /// Gets all shortcuts in the ToolStripDropDownItem and write it to the stringbuilder which is passed in byref
+        /// to be used by the parent method
+        /// - GetShortcuts(Control ctl, ref StringBuilder sb)
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="sb"></param>
+        public void GetShortcuts(ToolStripDropDownItem t, ref StringBuilder sb)
+        {
+            if (t.DropDownItems != null && t.DropDownItems.Count > 0)
+            {
+                foreach (ToolStripItem i in t.DropDownItems)
+                {
+                    if (i is ToolStripMenuItem)
+                    {
+                        GetShortcuts((ToolStripMenuItem)i, ref sb);
+                    }
+                }
+            }
+            else
+            {
+                if (t is ToolStripMenuItem)
+                {
+                    GetShortcuts((ToolStripMenuItem)t, ref sb);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Append shortcut to string builder. with some checking to ignore those without shortcuts
+        /// </summary>
+        /// <param name="strShortcut"></param>
+        /// <param name="strText"></param>
+        /// <param name="sb"></param>
+        private void AppendShortcut(string strShortcut, string strText, ref StringBuilder sb)
+        {
+            if (strShortcut != "None")
+            {
+                sb.AppendLine(FormatShortcutString(strShortcut, strText));
+            }
+        }
+
+        /// <summary>
+        /// Format the shortcut to be easily understandable. 
+        /// e.g. change D1 to 1, Oemtilde to ~
+        /// </summary>
+        /// <param name="shortcut"></param>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public string FormatShortcutString(string shortcut, string text)
+        {
+
+            List<string> l = new List<string>();
+            string[] strAry = shortcut.Split(',');
+
+            foreach (string s in strAry)
+                l.Add(s.Trim());
+
+
+            string strResult = "";
+            if (l.Contains("Control"))
+            {
+                l.Remove("Control");
+                strResult = "Ctl + ";
+            }
+
+            if (l.Contains("Alt"))
+            {
+                l.Remove("Alt");
+                strResult += "Alt + ";
+            }
+
+            if (l.Contains("Shift"))
+            {
+                l.Remove("Shift");
+                strResult += "Shift + ";
+            }
+
+            foreach (string s in l)
+            {
+                switch (s.Trim())
+                {
+                    case "Oemtilde":
+                        strResult += "~";
+                        break;
+
+                    case "D0":
+                    case "D1":
+                    case "D2":
+                    case "D3":
+                    case "D4":
+                    case "D5":
+                    case "D6":
+                    case "D7":
+                    case "D8":
+                    case "D9":
+                        strResult += s.Replace("D", "");
+                        break;
+
+                    default:
+                        strResult += s;
+                        break;
+                }
+            }
+
+            return string.Format("{0} \t\t==> {1}", strResult, text);
+        }
+
+    }
+}
---
+++ Libraries/UserFunctions.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CustomLibraries
+{
+    public class UserFunctions
+    {
+        ///// <summary>
+        ///// <para>Created By    : YUKUANG</para>
+        ///// <para>Created Date  : 15 Oct 2009</para>
+        ///// <para>Modified By   : -</para>
+        ///// <para>Modified Date : -</para>
+        ///// <para>---------------------------------------------------------------</para>
+        ///// <para></para>
+        ///// <para>Changes</para>
+        ///// <para>---------------------------------------------------------------</para>
+        ///// <para></para>
+        ///// <para>Description</para>
+        ///// <para>---------------------------------------------------------------</para>
+        ///// Gets Icon from Icon Folder
+        ///// </summary>
+        ///// <param name="strIconFile"></param>
+        ///// <returns></returns>
+        //public static Icon GetIcon(string strIconFile)
+        //{
+        //    try
+        //    {
+        //        return new Icon(strIconFile);
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return null;
+        //    }
+        //}
+
+        ///// <summary>
+        ///// Gets the real application path of the program.
+        ///// this will remove all parameters 
+        ///// e.g. 
+        ///// 
+        ///// From "C:\program files\my program.exe" -MyParam
+        ///// To "C:\program files\my program.exe"
+        ///// 
+        ///// </summary>
+        ///// <param name="strApplicationPath"></param>
+        ///// <returns></returns>
+        //public static string GetApplicationPart(string strApplicationPath)
+        //{
+        //    string strResult = strApplicationPath;
+        //    if (strResult[0] == '\"')
+        //    {
+        //        try
+        //        {
+        //            strResult = strResult.Substring(1, strResult.LastIndexOf("\"") - 1);
+        //        }
+        //        catch (Exception)
+        //        {
+        //            throw new Exception("Please ensure that the filepath is a valid path: " + strApplicationPath);
+        //        }
+        //    }
+        //    return strResult;
+        //}
+
+        ///// <summary>
+        ///// Gets the parameter part of the program.
+        ///// this will remove all parameters 
+        ///// e.g. 
+        ///// 
+        ///// From "C:\program files\my program.exe" -MyParam
+        ///// To -MyParam
+        ///// 
+        ///// </summary>
+        ///// <param name="strApplicationPath"></param>
+        ///// <returns></returns>
+        //public static string GetArgumentPart(string strApplicationPath)
+        //{
+        //    string strResult = strApplicationPath;
+
+        //    if ((strResult.LastIndexOf("\"") + 1) == 0)
+        //    {
+        //        strResult = string.Empty;
+        //    }
+        //    else
+        //    {
+        //        strResult = strResult.Substring(strResult.LastIndexOf("\"") + 1);
+        //    }
+
+        //    return strResult;
+        //}
+
+        ///// <summary>
+        ///// <para>Created By    : YUKUANG</para>
+        ///// <para>Created Date  : 24 Oct 2009</para>
+        ///// <para>Modified By   : -</para>
+        ///// <para>Modified Date : -</para>
+        ///// <para>---------------------------------------------------------------</para>
+        ///// <para></para>
+        ///// <para>Changes</para>
+        ///// <para>---------------------------------------------------------------</para>
+        ///// <para></para>
+        ///// <para>Description</para>
+        ///// <para>---------------------------------------------------------------</para>
+        ///// Executes/Start the program
+        ///// </summary>
+        ///// <param name="strApplicationToExecute"></param>
+        ////public static void ExecuteProgram(string strApplicationToExecute)
+        ////{
+        ////    //Exit if empty
+        ////    if (string.IsNullOrEmpty(strApplicationToExecute)) return;
+
+        ////    if (System.IO.File.Exists(GetApplicationPart(strApplicationToExecute)) || System.IO.Directory.Exists(GetApplicationPart(strApplicationToExecute)))
+
+        ////        //Try to execute file
+        ////        try
+        ////        {
+        ////            string strExecutable = GetApplicationPart(strApplicationToExecute);
+        ////            string strArgs = GetArgumentPart(strApplicationToExecute);
+
+        ////            System.Diagnostics.Process p = new System.Diagnostics.Process();
+        ////            p.StartInfo = new System.Diagnostics.ProcessStartInfo(strExecutable, strArgs);
+
+        ////            if (System.IO.File.Exists(strExecutable))
+        ////            {
+        ////                p.StartInfo.WorkingDirectory = System.IO.Directory.GetParent(strExecutable).FullName;
+        ////            }
+
+        ////            p.Start();
+        ////        }
+        ////        catch (Exception ex)
+        ////        {
+        ////            MessageBox.Show(ex.ToString());
+        ////        }
+        ////    else
+        ////        MessageBox.Show("Application/Directory does not exists.\nPlease ensure that the folder/executable exist.");
+        ////}
+
+
+        ///// <summary>
+        ///// <para>Created By    : YUKUANG</para>
+        ///// <para>Created Date  : 24 Oct 2009</para>
+        ///// <para>Modified By   : -</para>
+        ///// <para>Modified Date : -</para>
+        ///// <para>---------------------------------------------------------------</para>
+        ///// <para></para>
+        ///// <para>Changes</para>
+        ///// <para>---------------------------------------------------------------</para>
+        ///// <para></para>
+        ///// <para>Description</para>
+        ///// <para>---------------------------------------------------------------</para>
+        ///// Opens the program parent directory
+        ///// </summary>
+        ///// <param name="strApplicationToExecute"></param>
+        //public static void OpenProgramParent(string strApplicationToExecute)
+        //{
+        //    string strToExecute = CustomLibraries.UserFunctions.GetApplicationPart(strApplicationToExecute);
+
+        //    //Exit if empty
+        //    if (string.IsNullOrEmpty(strToExecute)) return;
+
+
+        //    if (System.IO.File.Exists(strToExecute) || System.IO.Directory.Exists(strToExecute))
+
+        //        //Try to execute file
+        //        try
+        //        {
+        //            System.Diagnostics.Process.Start(System.IO.Path.GetDirectoryName(strToExecute));
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            MessageBox.Show(ex.ToString());
+        //        }
+        //    else
+        //        MessageBox.Show("Application/Directory does not exists.\nPlease ensure that the folder/executable exist.");
+        //}
+
+        //public static string CreateUniqueFilename(string strFileSaveToLocation)
+        //{
+        //    if (!System.IO.File.Exists(strFileSaveToLocation))
+        //    {
+        //        return strFileSaveToLocation;
+        //    }
+        //    else
+        //    {
+        //        string folder = System.IO.Path.GetDirectoryName(strFileSaveToLocation);
+        //        string filename = System.IO.Path.GetFileNameWithoutExtension(strFileSaveToLocation);
+        //        string ext = System.IO.Path.GetExtension(strFileSaveToLocation);
+
+        //        string final = string.Concat(System.IO.Path.Combine(folder, filename), ext);
+        //        while (System.IO.File.Exists(final))
+        //        {
+        //            filename = System.IO.Path.GetFileNameWithoutExtension(strFileSaveToLocation) + "__" + DateTime.Now.Millisecond;
+        //            final = string.Concat(System.IO.Path.Combine(folder, filename), ext);
+        //        }
+        //        return final;
+        //    }
+        //}
+    }
+}
